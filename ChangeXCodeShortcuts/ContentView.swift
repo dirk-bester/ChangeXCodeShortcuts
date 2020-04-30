c16226c4a95d9c260e284e502cb4358803bb2a3c
dirk-bester <3698896+dirk-bester@users.noreply.github.com>
2020-04-30 13:10:15 -0700

    Initial Commit

---
+++ ChangeXCodeShortcuts/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ChangeXCodeShortcuts
+//
+//  Created by Dirk Bester on 4/30/20.
+//  Copyright © 2020 Dirk Bester. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
